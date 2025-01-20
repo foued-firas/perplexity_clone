1bca57b62ca710c2c52e8a86bda2414cb5817fb8
firas foued <firas.fouad@isimg.tn>
2025-01-20 03:33:04 +0100

    perplexity_clone

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:perplexity_clone/widegt/side_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    body: Row(
+      children: [
+       SideBar(),
+        Column(
+          children: [
+
+          ],
+        )
+      ],
+    ),
+    );
+  }
+}
