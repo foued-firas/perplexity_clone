1514af32e90c9d277310b0b63a5c861af7089bcd
firas foued <firas.fouad@isimg.tn>
2025-01-23 15:25:53 +0100

    perplexity_clone

--- server/sort_source_service.py
+++ server/sort_source_service.py
@@ -1,3 +1,12 @@
+from typing import List
+from sentence_transformers import SentenceTransformer
+
 class SortSourceService :
+
+    def __init__(self):
+        self.embedding_model = SentenceTransformer("all-miniLM-L6-v2")
+
     def sort_sources(self , query : str , search_results:List[dict]):
-        pass
+        query_embedding = self.embedding_model.encode(query)
+
+        print(query_embedding)
