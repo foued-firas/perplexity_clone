84726492c41a225cf8e18da884e426417b94b7f2
firas foued <firas.fouad@isimg.tn>
2025-01-26 19:25:56 +0100

    perplexity_clone

--- server/sort_source_service.py
+++ server/sort_source_service.py
@@ -9,4 +9,5 @@ class SortSourceService :
     def sort_sources(self , query : str , search_results:List[dict]):
         query_embedding = self.embedding_model.encode(query)
 
+
         print(query_embedding)
