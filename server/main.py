78291f4fe352e34b255b5e461f434c83de8111ac
firas foued <firas.fouad@isimg.tn>
2025-01-30 15:10:44 +0100

    perplexity_clone

--- server/main.py
+++ server/main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from pydantic_models.chat_body import ChatBody
+from services.sort_source_service import SortSourceService
 from services.search_service import SearchService
 
 
 app = FastAPI()
 
 search_service = SearchService()
+sort_source_service = SortSourceService()
 
 # chat 
 @app.post("/chat")
 def chat_endpoint(body :ChatBody):
     search_results = search_service.web_search(body.query)
-    print(search_results)
+    
+    # sort the sources
+    sort_source_service.sort_sources(body.query , search_results)
+    # generate the response using LLM
+    
     return body.query
 
 
--- server/services/sort_source_service.py
+++ server/services/sort_source_service.py
@@ -1,10 +1,11 @@
 from typing import List
 from sentence_transformers import SentenceTransformer
 
+
 class SortSourceService :
 
     def __init__(self):
-        self.embedding_model = SentenceTransformer("all-miniLM-L6-v2")
+        self.embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
     def sort_sources(self , query : str , search_results:List[dict]):
         query_embedding = self.embedding_model.encode(query)
