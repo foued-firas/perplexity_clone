8084ccb49f994672ada91d176fd68e88762a2340
firas foued <firas.fouad@isimg.tn>
2025-01-31 02:22:19 +0100

    perplexity_clone

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pydantic_models.chat_body import ChatBody
 from services.sort_source_service import SortSourceService
 from services.search_service import SearchService
@@ -10,6 +10,35 @@ app = FastAPI()
 search_service = SearchService()
 sort_source_service = SortSourceService()
 llm_service = LLMService()
+
+#chat websocket 
+@app.websocket("/ws/chat")
+async def websocket_chat_endpoint(websocket : WebSocket):
+    await websocket.accept()
+
+    try:
+        data =await websocket.receive_json()
+        query = data.get("query")
+        search_results = search_service.web_search(query)
+    
+        # sort the sources
+        sorted_results = sort_source_service.sort_sources(query , search_results)
+
+        await websocket.send_json({
+            'type': 'search_result',
+            'data': sorted_results
+        })
+    
+        # generate the response using LLM
+        response = llm_service.generate_response(query, sorted_results)
+
+        return response
+
+
+    except :
+        print("unexpected error occurred")
+    finally:
+        await websocket.close()    
 
 # chat 
 @app.post("/chat")
