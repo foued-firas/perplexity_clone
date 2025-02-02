b210acd0fe3d3de7022b7b14569ad5ead05c324a
firas foued <firas.fouad@isimg.tn>
2025-02-02 03:00:05 +0100

    perplexity_clone

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:perplexity_clone/services/chat_web_service.dart';
 import 'package:perplexity_clone/widgets/footer_bar.dart';
 import 'package:perplexity_clone/widgets/search_section.dart';
 import 'package:perplexity_clone/widgets/side_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+    ChatWebService().connect();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +26,21 @@ class HomePage extends StatelessWidget {
       children: [
        const SideBar(),
         Expanded(
+
           child: Column(
             children: [
-              const Expanded(child: SearchSection()),
+               Expanded(child: SearchSection()),
+             /* StreamBuilder(
+               stream: ChatWebService().contentStream,
+               builder: (context , snapshot){
+                if (snapshot.connectionState ==ConnectionState.waiting){
+                  return const Center(
+                    child:CircularProgressIndicator() ,
+                  );
+                }
+                return Text(snapshot.data()['content' ]);
+               },
+              ),*/
               Container(
                 padding: const EdgeInsets.symmetric(vertical: 16),
                child:  FooterBar(),
---
+++ lib/services/chat_web_service.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:web_socket_client/web_socket_client.dart';
+
+class ChatWebService {
+    static final _instance = ChatWebService._internal();
+    WebSocket? _socket ;
+    factory ChatWebService() => _instance;
+    //Every time you call ChatWebService(), it returns the same _instance instead of creating a new object
+    ChatWebService._internal();
+    //singleton 
+    final _searchResultController = StreamController<Map<String,dynamic>>();
+    final _contentController = StreamController<Map<String,dynamic>>();
+    Stream<Map<String,dynamic>> get searchResultStream =>
+    _searchResultController.stream;
+     Stream<Map<String,dynamic>> get contentStream =>
+    _contentController.stream;
+
+
+  void connect(){
+    _socket = WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
+
+    _socket!.messages.listen((message){
+        final data = jsonDecode(message);
+        if(data['type']== 'search_result'){
+        _searchResultController.add(data);
+
+        }else if (data['type'] == 'content'){
+            _contentController.add(data);
+        }
+    });
+  }
+
+  
+  void chat(String query){
+    
+    _socket!.send(json.encode({'query':query}));
+    print(query);
+  }
+}
